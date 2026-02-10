2a5aea444566835cf9ba2914f875d8c809e5411f
msfiqih <msfiqih@gmail.com>
2026-02-10 12:07:05 +0700

    fix login custom login

--- app/Filament/Auth/CustomLogin.php
+++ app/Filament/Auth/CustomLogin.php
@@ -2,14 +2,47 @@
 
 namespace App\Filament\Auth;
 
+use Dom\Comment;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\TextInput;
+use Filament\Http\Responses\Auth\LoginResponse;
 use Filament\Pages\Auth\Login;
 use Illuminate\Validation\ValidationException;
 
 class CustomLogin extends Login
 {
-    protected function getLoginFormComponent(): Component
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getUsernameFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getRememberFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            TextInput::make('username')
+                ->label('Username / ID Karyawan')
+                ->required()
+                ->autocomplete()
+                ->autofocus(),
+
+            TextInput::make('password')
+                ->label(__('Password'))
+                ->password()
+                ->required(),
+        ];
+    }
+
+    protected function getUsernameFormComponent(): Component
     {
         return TextInput::make('username')
             ->label(__('Username / ID Karyawan'))
@@ -32,5 +65,18 @@ class CustomLogin extends Login
         throw ValidationException::withMessages([
             'data.username' => __('filament-panels::pages/auth/login.messages.failed'),
         ]);
+
     }
+
+    // public function authenticate(): LoginResponse
+    // {
+    //     $credentials = $this->getCredentialsFromFormData($this->data);
+
+    //     if (!auth()->attempt($credentials)) {
+    //         dd($credentials, \App\Models\User::where('username', $credentials['username'])->first());
+    //     }
+
+    //     session()->regenerate();
+    // }
+
 }
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,6 +9,7 @@ use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
+use Filament\Pages\Auth\Login;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
