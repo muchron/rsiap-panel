fd137792ae223b7d590d1886cd9a4dfe5af5c06b
msfiqih <msfiqih@gmail.com>
2026-02-11 10:23:08 +0700

    fix article cover

--- app/Filament/Resources/ArticlesResource/Api/Transformers/ArticlesTransformer.php
+++ app/Filament/Resources/ArticlesResource/Api/Transformers/ArticlesTransformer.php
@@ -25,7 +25,7 @@ class ArticlesTransformer extends JsonResource
             'slug' => $this->resource->slug,
             'title' => $this->resource->title,
             'body' => $this->resource->body,
-            'cover' => env('APP_URL') . Storage::url($this->resource->cover),
+            'cover' => Storage::url($this->resource->cover),
             'author' => $this->resource->user->name,
             'category' => [
                 'name' => $this->resource->category->name,
